957423fd1d4c57e4b90c3f9201ac261bb2216bc5
WyattDun <149964892+WyattDun@users.noreply.github.com>
2023-11-09 13:01:46 -0500

    11-9-2023 Script

--- 11-5-23_Script.ino
+++ 11-5-23_Script.ino
@@ -4,22 +4,25 @@
 const char* ssid = "Wyatt";
 const char* password = "6uiyc3ytqi1k0";
 
-Servo myservo;
+Servo servo1;
+Servo servo2;
+Servo servo3;
 WiFiServer server(80);
 
 const int output5 = 5;
 const int externalButton = 2;
 
 bool servoOpen = false;
+int servoIndex = -1; // Variable to store the index of the open servo
+String number; // Declare number outside the if block
 
 unsigned long currentTime = millis();
 unsigned long previousTime = 0;
 const long timeoutTime = 2000;
 
-// Define your valid numbers
 String validNumbers[] = {
-  "123456789", // Add your valid 9-digit numbers here
-  "987654321",
+  "906588685", 
+  "906588686",
   "906588687"
 };
 
@@ -28,8 +31,9 @@ void setup() {
   pinMode(output5, OUTPUT);
   pinMode(externalButton, INPUT_PULLUP);
   digitalWrite(output5, LOW);
-  myservo.attach(0);
-  myservo.write(0);
+  servo1.attach(4); // Attach servo1 to pin GPIO4
+  servo2.attach(16); // Attach servo2 to pin GPIO16
+  servo3.attach(0); // Attach servo3 to pin GPIO0
 
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -61,13 +65,23 @@ void loop() {
         currentLine += c;
         if (c == '\n') {
           if (currentLine.startsWith("GET /submit?number=")) {
-            String number = currentLine.substring(19, 28); // Extract number from GET request
+            number = currentLine.substring(19, 28); // Extract number from GET request
             number.trim(); // Remove leading and trailing spaces
             number.replace("\n", ""); // Remove newline characters, if any
             Serial.println("Input Number: " + number);
             if (isValidNumber(number)) {
               Serial.println("Valid Number");
-              myservo.write(90);
+              // Determine the index of the valid number and open the corresponding servo
+              if (number == validNumbers[0]) {
+                servo1.write(90);
+                servoIndex = 0;
+              } else if (number == validNumbers[1]) {
+                servo2.write(90);
+                servoIndex = 1;
+              } else if (number == validNumbers[2]) {
+                servo3.write(90);
+                servoIndex = 2;
+              }
               servoOpen = true;
             } else {
               Serial.println("Invalid Number");
@@ -92,9 +106,26 @@ void loop() {
     client.stop();
   }
 
-  // Check for external button press to close the servo
+  // Check if the servo needs to move, prevents microstuddering and improves security
+  if (servoOpen) {
+    if (servoIndex == 0 && abs(servo1.read() - 90) > 2) {
+      servo1.write(90);
+    } else if (servoIndex == 1 && abs(servo2.read() - 90) > 2) {
+      servo2.write(90);
+    } else if (servoIndex == 2 && abs(servo3.read() - 90) > 2) {
+      servo3.write(90);
+    }
+  }
+
+  // Check for external button press to close the servo once the locker door is closed
   if (digitalRead(externalButton) == LOW && servoOpen) {
-    myservo.write(0);
+    if (servoIndex == 0) {
+      servo1.write(0);
+    } else if (servoIndex == 1) {
+      servo2.write(0);
+    } else if (servoIndex == 2) {
+      servo3.write(0);
+    }
     servoOpen = false;
   }
 }
